20cb81085c470c0f9f29aeadadbd0f464df2a9d7
dtwnik <dtwnik2@gmail.com>
2023-04-02 17:57:20 +0600

    second

--- tree_menu/main/urls.py
+++ tree_menu/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.allmenu, name='menu'),
     path('<str:menu_name>/', views.menu_page, name='menu_page'),
 ]
--- tree_menu/main/views.py
+++ tree_menu/main/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from django.shortcuts import render
 from .models import Menu
 
-
 # def menu_page(request):
 #     return render(request, 'main/menu.html', {
 #         'main_menu': Menu.objects.filter(name='main_menu').prefetch_related('children'),
@@ -13,6 +12,12 @@ from .models import Menu
 
 from django.shortcuts import render
 from .models import Menu
+
+
+def allmenu(request):
+    menu_items = Menu.objects.all()
+    context = {'menu': menu_items}
+    return render(request, 'main/index.html', context)
 
 
 def menu_page(request, menu_name):
