82130ee3382c8e46487628bd5b587303f4d228ba
dtwnik <72192760+dtwnik@users.noreply.github.com>
2023-04-02 17:58:20 +0600

    Update views.py
    delete unnecessary code

--- tree_menu/main/views.py
+++ tree_menu/main/views.py
@@ -1,16 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from .models import Menu
-
-# def menu_page(request):
-#     return render(request, 'main/menu.html', {
-#         'main_menu': Menu.objects.filter(name='main_menu').prefetch_related('children'),
-#         'footer_menu': Menu.objects.filter(name='footer_menu').prefetch_related('children'),
-#     })
-
-from django.shortcuts import render
 from .models import Menu
 
 
